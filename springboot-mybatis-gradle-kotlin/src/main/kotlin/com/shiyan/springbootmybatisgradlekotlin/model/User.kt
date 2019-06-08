b64afeea009a8ff51231820f300c2c60eb4515eb
kevin feng <kfeng631@gmail.com>
2019-06-08 10:55:18 +0800

    update package name

--- springboot-mybatis-gradle-kotlin/src/main/kotlin/com/shiyan/springbootmybatisgradlekotlin/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.easy.springboot.demo_springboot_with_mybatis.model
-
-/**
- *
- * @author kevinfeng
- * @date 2019-06-06
- */
-data class User(val id: Long, val username: String, val password: String)
